65eab1183c2e8073473dec2799c87b0d8c60b891
dinidc <d_ileri@hotmail.com>
2018-12-13 22:57:39 +0100

    CORS fix

--- server/app.js
+++ server/app.js
@@ -38,7 +38,7 @@ if(process.env.NODE_ENV !== 'production'){
     app.use(function (req, res, next) {
 
         // Website you wish to allow to connect
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:9890');
+        res.setHeader('Access-Control-Allow-Origin', '*');
       
         // Request methods you wish to allow
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
